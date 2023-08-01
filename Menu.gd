7f42a29ef45f791d5ac9c3f42a4275626253e9f5
carder46 <100096994+carder46@users.noreply.github.com>
2023-08-01 13:25:02 +1200

    didmenuandstart screen

--- Menu.gd
+++ Menu.gd
@@ -1,16 +1,7 @@
 extends Control
 
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://beginning.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-func _on_start_button_pressed():
-	get_tree().change_scene("res://World.tscn")
-45
+func _on_quit_pressed():
+	get_tree().quit()
--- World.gd
+++ World.gd
@@ -3,7 +3,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func _unhandled_input(event):
 	if Input.is_action_just_pressed('quit'):
